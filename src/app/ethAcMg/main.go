b627a68fcaedf8f70fe1bc34f50ff7e9de56f479
Cherry.chen <Cherry.chen@irootech.com>
2017-08-16 09:35:40 +0800

    add a eth keystore management

---
+++ src/app/ethAcMg/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+)
+
+func main() {
+	scryptN := keystore.StandardScryptN
+	scryptP := keystore.StandardScryptP
+	ks := keystore.NewKeyStore("keystore", scryptN, scryptP)
+	accs := ks.Accounts()
+	log.Println("accs:", accs)
+	ks.NewAccount("")
+}
